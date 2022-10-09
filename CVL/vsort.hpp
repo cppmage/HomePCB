150d9ceda1bb2040a272744ce9d8419ea441fa32
Disasming <disasming@mail.ru>
2022-10-10 00:46:02 +0300

    ->

--- CVL/vsort.hpp
+++ CVL/vsort.hpp
@@ -1,7 +1,6 @@
 #ifndef __vsort__
 	#define __vsort__
 		#include <iostream>
-		
 		#include <vector>
 		#include <string>
 		
@@ -21,19 +20,22 @@
 		using vpss_t = std::vector <pss_t>;	
 	
 		vpss_t StartPositionFinder(vs_iterator& ,vs_iterator& );
-	 	/*  * This function finding start positions in range first and last iterators of unsorted string vector of move positions,then counting all points in moving , sorting them and return . 
+	 	/*  * This function finding start positions in range first and last iterators of unsorted string vector of move positions ,
+		    * then counting all points in moving , sorting them and return . 
+		    
 			@ __firstparam -> reference on begin iterator in string vector  	
-	 		@ __secondparam -> reference on end iteraor in string vector
-			@ Return sorted vector of pairs of points counter and startposition
+	 	    @ __secondparam -> reference on end iteraor in string vector
+		    @ Return sorted vector of pairs of points counter and startposition
 	 	*/
 
 		vs_t LinesVectorCreate ( vs_iterator& ,vs_iterator& ,vpss_t& );
-		/*  * This function will create vector of move positions .This positions are present in strings coordinates.
-		    * Start positions string are ends "...D2*" and shpindel should be up.If string ends "...D1*",then shpindel should be down.
-		    @ __firstparam -> begin iterator of vector string type 
-		    @ __secondparam -> end iterator of vector string type
-			@ __thirdparam -> reference to vector of pair < repeat_point_counter,point_string > type
-			@ Return sorted vector of move positions.
+		/*  * This function will create vector of move positions. This positions are present in strings coordinates.
+		    * Start positions string are ends "...D2*" and shpindel should be up. If string ends "...D1*", then shpindel should be down.
+		    
+			@ __firstparam -> begin iterator of vector string type. 
+		    @ __secondparam -> end iterator of vector string type.
+		    @ __thirdparam -> reference to vector of pair of repeat_point_counter and start point string.
+		    @ Return sorted vector of move positions.
 		 */
 
 #endif
